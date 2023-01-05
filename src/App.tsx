8415d6c344ab08e3ffd67ea90b56463204d2cee3
edublkc <89402489+edublkc@users.noreply.github.com>
2023-01-04 21:39:11 -0300

    first section in the year

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
-    </div>
+    <>
+      <Header/>
+      <Dashboard/>
+      <GlobalStyle/>
+    </>
   );
 }
 
